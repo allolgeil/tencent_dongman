0b7d75852d07df5d0e35c1d114a2401c1469238f
allolgeil <2806354774@qq.com>
2017-08-17 16:34:21 +0800

    Update tencent_dongman.py

--- tencent_dongman.py
+++ tencent_dongman.py
@@ -5,4 +5,15 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 dcap = dict(DesiredCapabilities.PHANTOMJS)
 dcap['phantomjs.page.settings.userAgent'] = ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')
 browser = webdriver.PhantomJS(desired_capalilities=dcap)
-browser.get()
+browser.get('http://ac.qq.com/ComicView/index/id/518333/cid/1')
+a = browser.get_screenshot_as_file('D:/学习资料/tencent_dongman/test.jpg')
+for i in range(10):
+  js = 'window.scrollTo('+str(i*1280)+','+str((i+1)*1280)+')'
+  browser.execute_script(js)
+  time.sleep(1)
+print(browser.current_url)
+data = browser.page_source
+fh = open('D:/学习资料/tencent_dongman/dongman.html','w',encoding='utf-8')
+fh.write(data)
+fh.close()
+browser.quit()
