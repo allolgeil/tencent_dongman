acf9db9bfeed59ea9873376a066d7f3891b04500
allolgeil <2806354774@qq.com>
2017-08-17 16:48:02 +0800

    Update tencent_dongman.py

--- tencent_dongman.py
+++ tencent_dongman.py
@@ -7,7 +7,7 @@ dcap['phantomjs.page.settings.userAgent'] = ('Mozilla/5.0 (Windows NT 6.1; WOW64
 browser = webdriver.PhantomJS(desired_capalilities=dcap)
 browser.get('http://ac.qq.com/ComicView/index/id/518333/cid/1')
 a = browser.get_screenshot_as_file('D:/学习资料/tencent_dongman/test.jpg')
-for i in range(10):
+for i in range(20):
   js = 'window.scrollTo('+str(i*1280)+','+str((i+1)*1280)+')'
   browser.execute_script(js)
   time.sleep(1)
@@ -17,3 +17,12 @@ fh = open('D:/学习资料/tencent_dongman/dongman.html','w',encoding='utf-8')
 fh.write(data)
 fh.close()
 browser.quit()
+
+pat = '<>'
+allid = re.compile(pat).findall(data)
+for i im range(0,len(allid)):
+  thisurl = allid[i]
+  thisurl2 = thisurl.replace('amp;','')+'.jpg'
+  print(thisurl2)
+  localpath = 'D:/学习资料/tencent_dongman/'+str(i)+'.jpg'
+  urllib.request.urlretrieve(thisurl2.filename=localpath)
