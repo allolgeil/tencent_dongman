4a3968d0fdf89bd800bc39cb61860c3ae6183fce
allolgeil <2806354774@qq.com>
2017-08-23 05:56:57 +0800

    Update tencent_dongman.py

--- tencent_dongman.py
+++ tencent_dongman.py
@@ -1,28 +1,32 @@
+import urllib.request
 from selenium import webdriver
 import time
+import re
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 dcap = dict(DesiredCapabilities.PHANTOMJS)
 dcap['phantomjs.page.settings.userAgent'] = ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')
-browser = webdriver.PhantomJS(desired_capalilities=dcap)
-browser.get('http://ac.qq.com/ComicView/index/id/518333/cid/1')
-a = browser.get_screenshot_as_file('D:/学习资料/tencent_dongman/test.jpg')
+browser = webdriver.PhantomJS(desired_capabilities=dcap)
+browser.get('http://ac.qq.com/ComicView/index/id/547921/cid/1')
+a = browser.get_screenshot_as_file('D:/可删/test.jpg')
+
 for i in range(20):
   js = 'window.scrollTo('+str(i*1280)+','+str((i+1)*1280)+')'
   browser.execute_script(js)
   time.sleep(1)
+
 print(browser.current_url)
 data = browser.page_source
-fh = open('D:/学习资料/tencent_dongman/dongman.html','w',encoding='utf-8')
+fh = open('D:/可删/dongman.html','w',encoding='utf-8')
 fh.write(data)
 fh.close()
 browser.quit()
 
-pat = '<>'
+pat = '<img src="(http://ac.tc.qq.com/store_file_download.buid=.*?name=.*?).jpg"'
 allid = re.compile(pat).findall(data)
-for i im range(0,len(allid)):
+for i in range(0,len(allid)):
   thisurl = allid[i]
   thisurl2 = thisurl.replace('amp;','')+'.jpg'
   print(thisurl2)
-  localpath = 'D:/学习资料/tencent_dongman/'+str(i)+'.jpg'
-  urllib.request.urlretrieve(thisurl2.filename=localpath)
+  localpath = 'D:/可删/'+str(i)+'.jpg'
+  urllib.request.urlretrieve(thisurl2,filename=localpath)
